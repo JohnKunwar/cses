3f786d40a81148194341f820e14b7c93f835a890
Zero <johnkunwarofficial@gmail.com>
2026-02-16 16:50:29 +0545

    working invalid checker. Still doesn't work

--- two_knights.c
+++ two_knights.c
@@ -1,86 +1,52 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-int facto (int n)
-{
-    int res = 1;
-    for (int i = 2; i <= n; i++)
-        {
-            res = res * i;
-        }
 
-    return res;
-}
-int combination ( int n, int r)
+long long int invalid_move (long long int i, long long int j, long long int n)
 {
-    if (n == 1)
-        {
-            return 0;
-        }
-    int res = facto(n) / (facto(r) * facto(n - r));
-    return res;
-}
+    long long int invalid = 0;
 
-bool check (int i, int j, int n)
-{
-    int count = 0;
     if (((i + 2) < n) && ((j + 1) < n))
         {
-            count++;
+            invalid++;
         }
-    if (((i + 2) < n) && ((j - 1) < n) && (j - 1) > 0)
+    if (((i + 2) < n) && ((j - 1) < n) && (j - 1) >= 0)
         {
-            count++;
+            invalid++;
         }
-    if (((i - 2) < n) && ((j + 1) < n) && (i - 2) > 0)
+    if (((i - 2) < n) && ((j + 1) < n) && (i - 2) >= 0)
         {
-            count++;
+            invalid++;
         }
-    if (((i - 2) < n) && ((j - 1) < n) && (i - 2) > 0 && (j - 1) > 0)
+    if (((i - 2) < n) && ((j - 1) < n) && (i - 2) >= 0 && (j - 1) >= 0)
         {
-            count++;
+            invalid++;
         }
 
     if (((i + 1) < n) && ((j + 2) < n))
         {
-            count++;
+            invalid++;
         }
-    if (((i - 1) < n) && ((j + 2) < n) && (i - 1) > 0)
+    if (((i - 1) < n) && ((j + 2) < n) && (i - 1) >= 0)
         {
-            count++;
+            invalid++;
         }
-    if (((i + 1) < n) && ((j - 2) < n) && (j - 2) > 0)
+    if (((i + 1) < n) && ((j - 2) < n) && (j - 2) >= 0)
         {
-            count++;
+            invalid++;
         }
-    if (((i - 1) < n) && ((j - 2) < n) && (j - 2) > 0 && (i - 1) > 0)
+    if (((i - 1) < n) && ((j - 2) < n) && (j - 2) >= 0 && (i - 1) >= 0)
         {
-            count++;
+            invalid++;
         }
-    //    printf("x : %i y : %i\nx", i, j);
-    //printf("count: %i\n", count);
-    return count;
+
+    return invalid;
 }
+
 int main(void)
 {
-    int k;
-    scanf("%i", &k);
-
-
-
-    for (int l = 1; l <= k; l++)
-        {
-            int count = combination(l * l, 2);
-            for (int i = 0; i < l; i++)
-                {
-                    for (int j = 0; j < l; j++)
-                        {
-                            count = count - check(i, j, l);
-                        }
-                }
-            printf("%i\n", count);            
-        }
-
-
+    int n;
+    scanf("%i", &n);
+    printf("%lli ", invalid_move(0,0, 3));
     return 0;
 }
